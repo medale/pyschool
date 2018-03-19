eb7ab4f891b0e2bc0a78b6d9a7af292cfc58be75
medale <medale94@yahoo.com>
2018-03-18 21:54:44 -0400

    Iterables

--- docs/statements/Statements.md
+++ docs/statements/Statements.md
@@ -38,6 +38,51 @@ else:
 ```
 Source(Mark Lutz, Learning Python 5th edition, O'Reilly, 2013)
 
-# Iterations/Comprehensions
+# Iterations
 * for iteration over sequences (e.g. list, sets)
 * but also iterables: have `__next__` method and raise StopIteration error at end
+
+```python
+for c in my_list:
+    print(c)
+    
+for line in open('three_lines.txt'):
+    print(line, end='')
+```
+
+# List comprehension
+```python
+# Most general syntax
+new_list = [str(elem) for elem in old_list if test]
+
+l = range(100)
+# 1. call next on iterator over old_list to get new elem
+# 2. check if elem passes the filter's test
+# 3. If pass - return elem as part of the new list
+# 4. If fail - goto 1
+new_l = [num for num in l if num % 2 == 0]
+```
+
+# Set and dictionary comprehensions
+```python
+letters = ['A', 'c', 'C', 'D', 'D', 'e']
+unique_uppers = {letter for letter 
+                   in letters if letter.isupper()}
+
+lowers = [letter for letter in string.ascii_lowercase]
+
+# ord(c) returns ascii value for c 
+# chr(num) converts num to character
+ascii_vals = [ord(letter) for letter in lowers]
+
+ascii_dict = {letter: ascii for letter,ascii in zip(lowers, ascii_vals)}
+```
+
+# Functions on iterables
+* list, set, dict
+* enumerate - creates two-tuple with first element (0, first_iter),
+* zip
+* map
+* sorted
+* join
+* sum, any, all, max, min
--- learning/statements/iterations.py
+++ learning/statements/iterations.py
@@ -1,4 +1,5 @@
 import os
+import string
 
 def iterations():
 
@@ -19,20 +20,75 @@ def iterations():
         # each line has \n - end '' removes
         print(line, end='')
 
-
+    # behind the scenes - calls to __iter__/__next__ until StopIteration
     f = open('three_lines.txt')
-    iter = f.__iter__()
+    # a file object is an iterator so we don't need
+    # iter = f.__iter__()
     done = False
     while not done:
         try:
-            line = iter.__next__()
+            line = f.__next__()
             if not line:
                 done = True
             print(line, end='')
         except StopIteration:
-            print("It's the end, my friend")
+            print("\nIt's the end, my friend")
             break
+
+    # stepping through a dictionary
+    d = {'a': 1, 'b': 2, 'c': 3}
+    for key in d:
+        print(key, d[key])
+
+    # stepping through iterables - like enumerate
+    l = ['a', 'b', 'c']
+    # create (0, 'a') etc. tuples
+    e = enumerate(l)
+    iter = iter(e)
+    element1 = next(iter)
+
+
+def comprehensions():
+    l = range(100)
+    # new_list = [str(elem) for elem in old_list if test]
+    # 1. call next on iterator over old_list to get new elem
+    # 2. check if elem passes the filter's test
+    # 3. If pass - return elem as part of the new list
+    # 4. If fail - goto 1
+    new_l = [num for num in l if num % 2 == 0]
+
+    letters = ['A', 'c', 'C', 'D', 'D', 'e']
+    unique_uppers = {letter for letter in letters if letter.isupper()}
+
+    lowers = [letter for letter in string.ascii_lowercase]
+
+    # ord(c) returns ascii value for c
+    # chr(num) converts num to character
+    ascii_vals = [ord(letter) for letter in lowers]
+
+    ascii_dict = {letter: ascii for letter,ascii in zip(lowers, ascii_vals)}
+
+
+def functions_on_iterables():
+    # list, set, dict (iterable over two-tuples), enumerate
+    dupes = 'aabbccdd'
+    ldupes = list(dupes)
+    sdupes = set(dupes)
+    ddupes = dict(enumerate(dupes))
+
+    # zip returns iterator - convert to list
+    pairs = list(zip('abcd','ghi'))
+
+    # apply method upper of str objects for each letter in dupes
+    uppers = list(map(str.upper, dupes))
+
+    order = sorted(sdupes)
+
+    joined = ','.join(dupes)
+
+    # sum, any, all, max, min
 
 
 if __name__== "__main__":
     iterations()
+    comprehensions()
